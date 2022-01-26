18921a903f8dc64627628e1604c2e6941d0b71ec
michalmpeis <alexbitsos@gmail.com>
2022-01-26 21:22:55 +0200

    Added the application code worked up to 26/01/2022. Contains a home page, maps, and search bar. Application works.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,10 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // Google Maps API key
+                GMSServices.provideAPIKey("AIzaSyCFX7sAk_ZmnwI7rC2_4e9HKK2dp8APZ8c")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
